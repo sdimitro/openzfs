f52ddae91200b09145c5c776f7e34fb1d4c6aa4c
George.Wilson <george.wilson@delphix.com>
2011-04-25 10:51:22 -0700

    10946 appliance/installer/etc/syslog.conf cannot be in delphix-gate 11013 modified versions of nsswitch.dns, .profile, and .bashrc should live in os gate

--- usr/src/cmd/nsadmin/dot-profile.sh
+++ usr/src/cmd/nsadmin/dot-profile.sh
@@ -1,10 +1,33 @@
 #
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+# Copyright (c) 2011 by Delphix. All rights reserved.
+#
+
+#
 # Simple profile places /usr/gnu/bin at front,
 # adds /usr/X11/bin, /usr/sbin and /sbin to the end.
 #
 # Use less(1) as the default pager for the man(1) command.
 #
-export PATH=/usr/gnu/bin:/usr/bin:/usr/sbin:/sbin
+export PATH=/usr/ccs/bin:/usr/local/bin:/usr/gnu/bin:/usr/bin:/usr/sbin:/sbin
 export PAGER="/usr/bin/less -ins"
 
 #

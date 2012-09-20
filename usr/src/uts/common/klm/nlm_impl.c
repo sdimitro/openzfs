59d9450b8019eec517be274f2d87a6ca63777030
Jeremy Jones <jeremy@delphix.com>
2012-09-20 10:50:17 -0700

    19942 RAC Provision Failed to recreate control file.

--- usr/src/uts/common/klm/nlm_impl.c
+++ usr/src/uts/common/klm/nlm_impl.c
@@ -102,12 +102,12 @@ struct nlm_knc {
 #define	NLM_BMAP_WORDS  (NLM_BMAP_NITEMS / BT_NBIPUL)
 
 /*
- * Given an interger x, the macro is returned
+ * Given an interger x, the macro returs
  * -1 if x is negative,
  *  0 if x is zero
  *  1 if x is positive
  */
-#define	SIGN(x) (((x) < 0) - ((x) > 0))
+#define	SIGN(x) (((x) > 0) - ((x) < 0))
 
 #define	ARRSIZE(arr)	(sizeof (arr) / sizeof ((arr)[0]))
 #define	NLM_KNCS	ARRSIZE(nlm_netconfigs)
@@ -1475,11 +1475,11 @@ nlm_netbuf_addrs_cmp(struct netbuf *nb1, struct netbuf *nb2)
 
 	switch (na1->sa.sa_family) {
 	case AF_INET:
-		res = bcmp(&na1->sin.sin_addr, &na2->sin.sin_addr,
+		res = memcmp(&na1->sin.sin_addr, &na2->sin.sin_addr,
 		    sizeof (na1->sin.sin_addr));
 		break;
 	case AF_INET6:
-		res = bcmp(&na1->sin6.sin6_addr, &na2->sin6.sin6_addr,
+		res = memcmp(&na1->sin6.sin6_addr, &na2->sin6.sin6_addr,
 		    sizeof (na1->sin6.sin6_addr));
 		break;
 	default:

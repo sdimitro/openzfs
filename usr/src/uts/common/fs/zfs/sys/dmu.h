77747157ab66cc028d62a394c73a1a1920793891
Matthew Ahrens <mahrens@delphix.com>
2014-04-23 03:21:25 +0000

    32975 files sometimes can't be removed from a full filesystem

--- usr/src/uts/common/fs/zfs/sys/dmu.h
+++ usr/src/uts/common/fs/zfs/sys/dmu.h
@@ -562,6 +562,7 @@ dmu_tx_t *dmu_tx_create(objset_t *os);
 void dmu_tx_hold_write(dmu_tx_t *tx, uint64_t object, uint64_t off, int len);
 void dmu_tx_hold_free(dmu_tx_t *tx, uint64_t object, uint64_t off,
     uint64_t len);
+void dmu_tx_hold_netfree(dmu_tx_t *tx);
 void dmu_tx_hold_zap(dmu_tx_t *tx, uint64_t object, int add, const char *name);
 void dmu_tx_hold_bonus(dmu_tx_t *tx, uint64_t object);
 void dmu_tx_hold_spill(dmu_tx_t *tx, uint64_t object);

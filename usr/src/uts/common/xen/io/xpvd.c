09846dfe2cfc30fe54d80637aea0aa38b64127ed
Paul Dagnelie <paul.dagnelie@delphix.com>
2014-05-09 11:22:51 -0700

    33942 Creating xdf.conf file causes the kernel to panic on boot

--- usr/src/uts/common/xen/io/xpvd.c
+++ usr/src/uts/common/xen/io/xpvd.c
@@ -25,6 +25,7 @@
  */
 /*
  * Copyright 2012 Garrett D'Amore <garrett@damore.org>.  All rights reserved.
+ * Copyright (c) 2014 by Delphix. All rights reserved.
  */
 
 /*
@@ -762,11 +763,19 @@ xpvd_name_child(dev_info_t *child, char *addr, int addrlen)
 	ddi_prop_free(domain);
 
 	/*
-	 * Use "unit-address" property (frontend/softdev drivers).
+	 * Use "vdev" and "unit-address" properties (frontend/softdev drivers).
+	 * At boot time, only the vdev property is available on xdf disks.
 	 */
 	if (ddi_prop_lookup_string(DDI_DEV_T_ANY, child, DDI_PROP_DONTPASS,
-	    "unit-address", &prop_str) != DDI_PROP_SUCCESS)
-		return (DDI_FAILURE);
+	    "unit-address", &prop_str) != DDI_PROP_SUCCESS) {
+		if (ddi_prop_lookup_int_array(DDI_DEV_T_ANY, child, DDI_PROP_DONTPASS,
+		    "vdev", &vdev, &nvdev) != DDI_PROP_SUCCESS)
+			return (DDI_FAILURE);
+		ASSERT(nvdev == 1);
+		(void) snprintf(addr, addrlen, "%d", vdev[0]);
+		ddi_prop_free(vdev);
+		return (DDI_SUCCESS);
+	}
 	(void) strlcpy(addr, prop_str, addrlen);
 	ddi_prop_free(prop_str);
 	return (DDI_SUCCESS);
@@ -783,6 +792,10 @@ xpvd_initchild(dev_info_t *child)
 	 */
 	if (ndi_dev_is_persistent_node(child) == 0) {
 		ddi_set_parent_data(child, NULL);
+		if (xpvd_name_child(child, addr, sizeof (addr)) != DDI_SUCCESS) {
+			return (DDI_FAILURE);
+		}
+		ddi_set_name_addr(child, addr);
 
 		/*
 		 * Try to merge the properties from this prototype

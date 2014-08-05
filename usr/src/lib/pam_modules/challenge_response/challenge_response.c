cc46cbdbb6bd3c51831b4c384a7f35a8b4063b45
Max Grossman <max.grossman@delphix.com>
2014-08-05 15:12:34 +0000

    36951 improper use of sizeof compromises challenge-response PAM

--- usr/src/lib/pam_modules/challenge_response/challenge_response.c
+++ usr/src/lib/pam_modules/challenge_response/challenge_response.c
@@ -275,7 +275,7 @@ calculate_response(char *challenge, char *key,
 	hmac_offset = hmac[OFFSET_INDEX] & 0xF;
 	response_val = toint_truncate(hmac + hmac_offset,
 	    RESPONSE_LENGTH);
-	(void) snprintf(response, sizeof (response), "%u", response_val);
+	(void) snprintf(response, RESPONSE_LENGTH + 1, "%u", response_val);
 	return (0);
 }
 
@@ -436,6 +436,11 @@ pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv)
 		 * Validate the calculated response against the entered
 		 * response.
 		 */
+		if (strlen(user_response) != strlen(correct_response_val_str)) {
+			__pam_log(LOG_AUTH | LOG_DEBUG,
+			    "pam_challenge_response: wrong response length");
+			return (PAM_AUTH_ERR);
+		}
 		if (strncmp(user_response, correct_response_val_str,
 			strlen(correct_response_val_str)) == 0) {
 			return (PAM_SUCCESS);

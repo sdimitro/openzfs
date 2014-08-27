786c4345aa06e93f7ef89501bcd74f7794318c73
Paul Dagnelie <pcd@delphix.com>
2014-08-27 09:29:28 -0700

    37378 zfs send can no longer be cancelled with ctrl-c

--- usr/src/uts/common/fs/zfs/dmu_send.c
+++ usr/src/uts/common/fs/zfs/dmu_send.c
@@ -1164,6 +1164,8 @@ dmu_send_impl(void *tag, dsl_pool_t *dp, dsl_dataset_t *to_ds,
 			err = do_dump(to_ds, to_data, dsp);
 			to_data = get_next_record(&to_arg.q, to_data);
 		}
+		if (issig(JUSTLOOKING) && issig(FORREAL))
+	                err = EINTR;
 	}
 
 	if (err != 0) {

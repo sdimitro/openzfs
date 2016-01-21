555b788f52c96abc17b9088714dc466c083f0f74
Paul Dagnelie <pcd@delphix.com>
2016-01-21 08:32:05 -0800

    DLPX-42198 kmem leak in receive_read_payload_and_next_header

--- usr/src/uts/common/fs/zfs/dmu_send.c
+++ usr/src/uts/common/fs/zfs/dmu_send.c
@@ -4849,6 +4849,8 @@ dmu_recv_begin(char *tofs, char *tosnap, dmu_replay_record_t *drr_begin,
 		    KM_SLEEP);
 		kmem_free(payload, payloadlen);
 		if (err != 0) {
+			kmem_free(drc->drc_next_rrd,
+			    sizeof (*drc->drc_next_rrd));
 			return (err);
 		}
 	}
@@ -4869,6 +4871,8 @@ dmu_recv_begin(char *tofs, char *tosnap, dmu_replay_record_t *drr_begin,
 	}
 
 	if (err != 0) {
+		kmem_free(drc->drc_next_rrd,
+		    sizeof (*drc->drc_next_rrd));
 		nvlist_free(drc->drc_begin_nvl);
 	}
 	return (err);
